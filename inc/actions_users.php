8d9d310aa6a0f8367494eec62fe0d2c1527273d2
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-06-06 15:49:36 -0700

    create inc/actions_users based on actions_tasks - create works

---
+++ inc/actions_users.php
@@ -0,0 +1,52 @@
+<?php
+require_once "bootstrap.php";
+
+$action = request()->get('action');
+$userId = request()->get('user_id');
+$username = request()->get('username');
+$password = request()->get('password');
+$confirmPassword = request()->get('confirm_password');
+
+echo $action;
+echo "<br>";
+echo $username;
+echo "<br>";
+echo $password;
+echo "<br>";
+
+$url="../user_list.php";
+if (request()->get('filter')) {
+	$url.="?filter=".request()->get('filter');
+}
+
+switch ($action) {
+	case "add":
+		if (empty($username)) {
+			$session->getFlashBag()->add('error', 'Please enter a user');
+			redirect('/user.php');
+		} elseif ($password != $confirmPassword) {
+			$session->getFlashBag()->add('error', 'Passwords do NOT match');
+			redirect('/user.php');
+		} elseif (!empty(findUserByUsername($username))) {
+			$session->getFlashBag()->add('error', 'User already exists');
+			redirect('/user.php');
+		} elseif (createUser($username, $password)) {
+			$session->getFlashBag()->add('success', 'New User Added');
+			redirect('/');
+		}
+		break;
+	case "update":
+		if (updateUser($userId, $password)) {
+			$session->getFlashBag()->add('success', 'User Updated');
+		} else {
+			$session->getFlashBag()->add('error', 'Could NOT update user');
+		}
+		break;
+	case "delete":
+		if (deleteUser($username_id)) {
+			$session->getFlashBag()->add('success', 'User Deleted');
+		} else {
+			$session->getFlashBag()->add('error', 'Could NOT Delete User');
+		}
+		break;
+}
