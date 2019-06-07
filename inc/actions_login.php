242972f7ff4f6c7908f098b697d5e608086b9a05
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-06-06 19:20:36 -0700

    successfully failing to login with incorrect credentials...now ;)

--- inc/actions_login.php
+++ inc/actions_login.php
@@ -8,13 +8,13 @@ if (empty($user)) {
   redirect('/login.php');
 }
 
-if (!password_verify(request()->get('password'), $user['password'])) {
+if (password_verify(request()->get('password'), $user['password'])) {
+  $session->set('auth_logged_in', true);
+  $session->set('auth_user_id', (int) $user['id']);
+  $session->set('auth_roles', (int) $user['role_id']);
+  $session->getFlashBag()->add('success', 'You are logged in.');
+  redirect('/');
+} else {
   $session->getFlashBag()->add('error', 'Either the username or password does not match our records.');
+  redirect('/login.php');
 }
-
-$session->set('auth_logged_in', true);
-$session->set('auth_user_id', (int) $user['id']);
-$session->set('auth_roles', (int) $user['role_id']);
-
-$session->getFlashBag()->add('success', 'You are logged in.');
-redirect($_SERVER['HTTP_REFERER']);
