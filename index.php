c324e354839900f5c8092784bc53dd022841ad24
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-06-12 19:27:02 -0700

    fix index.php weirdly missing a bunch of closing tags at the end 🤔

--- index.php
+++ index.php
@@ -6,34 +6,38 @@ $page = null;
 
 include 'inc/header.php';
 ?>
-    <div class="col-container actions-container">
 
-      <h1>Welcome</h1>
-      <p class="actions-copy">What would you like to do today?</p>
-      <div class="actions-wrapper">
-        <ul class="actions">
-            <li class="actions-item">
-                <a class="actions-link" href="task_list.php">
-              <span class="actions-icon">
-                <svg viewbox="0 0 64 64"><use xlink:href="#report_icon"></use></svg>
-              </span>
-                    View Tasks
-                </a>
-            </li>
+  <div class="col-container actions-container">
+    <h1>Welcome</h1>
+    <p class="actions-copy">What would you like to do today?</p>
+    <div class="actions-wrapper">
+      <ul class="actions">
           <li class="actions-item">
-            <a class="actions-link" href="task.php">
-              <span class="actions-icon">
-                <svg viewbox="0 0 64 64"><use xlink:href="#task_icon"></use></svg>
-              </span>
-              Add Task
-            </a>
+              <a class="actions-link" href="task_list.php">
+            <span class="actions-icon">
+              <svg viewbox="0 0 64 64"><use xlink:href="#report_icon"></use></svg>
+            </span>
+                  View Tasks
+              </a>
           </li>
-          <li class="actions-item">
-            <a class="actions-link" href="user.php">
-              <span class="actions-icon">
-                <svg viewbox="0 0 64 64"><use xlink:href="#user_icon"></use></svg>
-              </span>
-                Register
-		</div>
+        <li class="actions-item">
+          <a class="actions-link" href="task.php">
+            <span class="actions-icon">
+              <svg viewbox="0 0 64 64"><use xlink:href="#task_icon"></use></svg>
+            </span>
+            Add Task
+          </a>
+        </li>
+        <li class="actions-item">
+          <a class="actions-link" href="user.php">
+            <span class="actions-icon">
+              <svg viewbox="0 0 64 64"><use xlink:href="#user_icon"></use></svg>
+            </span>
+              Register
+          </a>
+        </li>
+      </ul>
+    </div>
+	</div>
 
 <?php include("inc/footer.php"); ?>
